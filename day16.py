c8b42058139bfdedda1cb2c51507816cf87e4fa6
z4tz <z4tz87@gmail.com>
2023-12-16 15:33:14 +0100

    move to dictionary based grid

--- day16.py
+++ day16.py
@@ -1,69 +1,53 @@
 import os
 from inputreader import aocinput
-from collections import deque, defaultdict
+from collections import deque
 
 
-def energized_tiles(data: list[str], size=None, start: tuple[complex, complex] = (0 + 0j, 1)) -> int:
-    if size is None:
-        size = (len(data[0]), len(data))
-    to_move: deque[tuple[complex, complex]] = deque([start])
+def energized_tiles(data: dict[complex, str], start: tuple[complex, complex] = (0 + 0j, 1)) -> int:
+    branches: deque[tuple[complex, complex]] = deque([start])
     visited = set()
-    while to_move:
-        location, direction = to_move.pop()
-        if 0 > location.real or size[0] <= location.real or 0 > location.imag or size[1] <= location.imag or (
-                location, direction) in visited:
-            continue  # skip value outside field or already visited
-        tile = data[int(location.imag)][int(location.real)]
-        visited.add((location, direction))
-        if tile == '.':
-            to_move.append((location + direction, direction))
-        elif tile == '/':
-            if direction.real:
-                direction *= -1j
-            else:
-                direction *= 1j
-            to_move.append((location + direction, direction))
-        elif tile == '\\':
-            if direction.real:
-                direction *= 1j
-            else:
-                direction *= -1j
-            to_move.append((location + direction, direction))
-        elif tile == '|':
-            if direction.real:
-                to_move.append((location + 1j, 1j))
-                to_move.append((location + -1j, -1j))
-            else:
-                to_move.append((location + direction, direction))
-        elif tile == '-':
-            if direction.imag:
-                to_move.append((location + 1, 1))
-                to_move.append((location + -1, -1))
-            else:
-                to_move.append((location + direction, direction))
+    while branches:
+        location, direction = branches.pop()
+        while (location, direction) not in visited:
+            tile = data.get(location)
+            if tile is None:
+                break
+            visited.add((location, direction))
+            if tile == '/':
+                direction = -complex(direction.imag, direction.real)
+            elif tile == '\\':
+                direction = complex(direction.imag, direction.real)
+            elif tile == '|':
+                if direction.real:
+                    direction = -1j
+                    branches.append((location + 1j, 1j))
+            elif tile == '-':
+                if direction.imag:
+                    direction = 1
+                    branches.append((location - 1, -1))
+            location += direction
 
     energized = len(set([location for location, direction in visited]))
     return energized
 
 
-def best_configuration(data: list[str]) -> int:
+def best_configuration(data: list[str]) -> tuple[int, int]:
     results = []
-    size = (len(data[0]), len(data))
+    size = (len(data[0].strip()), len(data))
+    data = {complex(x, y): char for y, line in enumerate(data) for x, char in enumerate(line.strip())}
     for x in range(size[0]):
-        results.append(energized_tiles(data, size, start=(x+0j, 1j)))
-        results.append(energized_tiles(data, size, start=(complex(x, size[1]-1), -1j)))
+        results.append(energized_tiles(data, start=(x+0j, 1j)))
+        results.append(energized_tiles(data, start=(complex(x, size[1]-1), -1j)))
     for y in range(size[1]):
-        results.append(energized_tiles(data, size, start=(complex(0, y), 1)))
-        results.append(energized_tiles(data, size, start=(complex(size[0]-1, y), -1)))
-    return max(results)
+        results.append(energized_tiles(data, start=(complex(0, y), 1)))
+        results.append(energized_tiles(data, start=(complex(size[0]-1, y), -1)))
+    return energized_tiles(data), max(results)
 
 
 def main(day: int):
     data = aocinput(day)
-    data = [line.strip() for line in data]
-    result = energized_tiles(data)
-    results2 = best_configuration(data)
-    print(result, results2)
+    results = best_configuration(data)
+    print(results)
 
 
 if __name__ == '__main__':
